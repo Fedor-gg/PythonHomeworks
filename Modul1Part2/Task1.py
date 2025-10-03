b1971ea97445563b9e3a09696e896aa6e1ee030f
Fedor-gg <korolev12547@gmail.com>
2025-10-03 13:15:51 +0300

    Add files via upload

---
+++ Modul1Part2/Task1.py
@@ -0,0 +1,5 @@
+number1 = int(input("введите число: "))
+number2 = int(input("введите число: "))
+number3 = int(input("введите число: "))
+print(number1 + number2 + number3)
+print(number1 * number2 * number3)
